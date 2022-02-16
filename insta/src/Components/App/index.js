0a5053fe448e492bc240d39f28ed80ef5b09d108
danmartin98 <danmartin98@icloud.com>
2022-02-16 20:00:09 +0000

    login page done, routes set up

--- insta/src/Components/App/index.js
+++ insta/src/Components/App/index.js
@@ -1,11 +1,15 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const Login = lazy(() => import("../../pages/login"));
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <p>Hello</p>
-      </div>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
---
+++ insta/src/pages/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <div>
+      <p>I am the login page</p>
+    </div>
+  );
+}
